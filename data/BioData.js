dd71ad22cd31d97d21907186f96c881bbd237b50
Chintan Udani <chintanudani2000@gmail.com>
2023-04-12 10:56:05 +0530

    updated biolinks

--- data/BioData.js
+++ data/BioData.js
@@ -1,25 +1,19 @@
 // date: 1 Aug, 2022
-const bioData = [
-    {
-        name: 'nxt lnk',
-        username: 'nxtlnk.vercel.app',
-        url: 'https://nxtlnk.vercel.app',
-        titleImg: true,
-        avatar: '/avatar.png',
-        nftAvatar: true,
-        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
-        descShow: true,
-        subdesc: 'Use this template and deploy on vercel for free',
-        subdescShow: true,
-        newProductUrl: 'https://3dicons.co',
-        newProduct: true,
-        author: 'realvjy',
-        authorURL: 'https://twitter.com/realvjy',
-        footerText: 'made by'
-    },
-];
+const bioData = [{
+    name: "nxtlnk",
+    username: "nxtlnk.netlify.app",
+    url: "https://nxtlnk.netlify.app",
+    titleImg: true,
+    avatar: "/avatar.png",
+    nftAvatar: true,
+    description: "Bio link of Chintan Udani ",
+    descShow: true,
+    subdesc: "",
+    subdescShow: true,
+    newProductUrl: "https://3dicons.co",
+    newProduct: true,
+
+    footerText: "made by Chintan with 💖",
+}, ];
 
 export default bioData;
-
-
-
--- data/LinksData.js
+++ data/LinksData.js
@@ -2,108 +2,31 @@
 const webLinks = [
     // All social profile
     {
-        title: 'Twitter',
-        url: 'https://twitter.com/realvjy',
-        type: 'social',
-        icon: '/twitter.svg',
-        on: true
-    },
-    {
         title: 'Instagram',
-        url: 'https://instagram.com/realvjy',
+        url: 'https://instagram.com/chintan.ux',
         type: 'social',
         icon: '/insta.svg',
         on: true
     },
     {
-        title: 'Dribbble',
-        url: 'https://dribbble.com/realvjy',
+        title: 'Techpaper',
+        url: 'https://techpaper.netlify.app',
         type: 'social',
-        icon: '/dribbble.svg',
-        on: false
-    },
-    {
-        title: 'Medium',
-        url: 'https://medium.com/@realvjy',
-        type: 'social',
-        icon: '/medium.svg',
+        icon: '',
         on: false
     },
     {
         title: 'Github',
-        url: 'https://github.com/realvjy',
+        url: 'https://github.com/chintan-udani',
         type: 'social',
         icon: '/github.svg',
         on: true
     },
     {
-        title: 'Youtube',
-        url: 'https://youtube.com/realvjy',
-        type: 'social',
-        icon: '/youtube.svg',
-        on: false
-    },
-    {
-        title: 'Figma',
-        url: 'https://figma.com/@realvjy',
-        type: 'social',
-        icon: '/figma.svg',
-        on: true
-    },
-    {
-        title: 'Behance',
-        url: 'https://behance.net/realvjy',
-        type: 'social',
-        icon: '/behance.svg',
-        on: false
-    },
-    {
         title: 'Linkedin',
-        url: 'https://www.linkedin.com/in/realvjy/',
+        url: 'https://www.linkedin.com/in/chintan-udani/',
         type: 'social',
         icon: '/linkedin.svg',
-        on: false
-    },
-    {
-        title: 'Mastodon',
-        url: 'https://mastodon.online/@realvjy',
-        type: 'social',
-        icon: '/mastodon.svg',
-        on: true
-    },
-    {
-        title: 'Hive Social',
-        url: 'https://www.hivesocial.app/?@realvjy',
-        type: 'social',
-        icon: '/hive.svg',
-        on: false
-    },
-    {
-        title: 'Post',
-        url: 'https://www.post.news/realvjy',
-        type: 'social',
-        icon: '/post.svg',
-        on: false
-    },
-    {
-        title: 'foundation',
-        url: 'https://vjy.me',
-        type: 'nft',
-        icon: '/foundation.svg',
-        on: false
-    },
-    {
-        title: 'opensea',
-        url: 'https://vjy.me',
-        type: 'nft',
-        icon: '/opensea.svg',
-        on: false
-    },
-    {
-        title: 'new collection',
-        url: 'https://vjy.me',
-        type: 'nft',
-        icon: '/opensea.svg',
         on: false
     },
     // Install section
@@ -137,24 +60,24 @@ const webLinks = [
     },
 
     {
-        title: 'Buy Me a Coffee',
-        url: 'https://www.buymeacoffee.com/realvjy',
-        type: 'other',
-        icon: '/bmc.png',
+        title: '',
+        url: '',
+        type: '',
+        icon: '',
         on: true
     },
 
     {
-        title: 'link demo',
-        url: 'https://vjy.me/lnk',
-        type: 'other',
-        icon: '/vjy.png',
+        title: '',
+        url: '',
+        type: '',
+        icon: '',
         on: true
     },
 
     {
-        title: 'maker website',
-        url: 'https://vjy.me',
+        title: 'Portfolio',
+        url: 'https://chintanudani.tech',
         type: 'other',
         icon: '/vjy.png',
         on: true
@@ -162,5 +85,3 @@ const webLinks = [
 ];
 
 export default webLinks;
-
-
